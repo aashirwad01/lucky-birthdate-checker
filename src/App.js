69ac1b225147d4586aef088d1973aaf6e89c2dcd
AASHIRWAD KUMAR <aashirwadkumar159@gmail.com>
2021-09-27 14:13:04 +0530

    feat-added feature to know if you're lucky.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
+
+  const [state, setstate] = useState("");
+  const [state2, set2] = useState("");
+  const [state3, set3] = useState("");
+  
+  function luckypicker(event){
+    // console.log(event.target.value);
+    setstate(event.target.value);
+  }
+
+  function datepicker(event){
+    // console.log(event.target.value);
+    set2(event.target.value);
+  }
+  
+  function clickhandler(){
+    var str=state2;
+    var ar=str.split("");
+    // console.log(ar);
+    var s=0;
+    for(var i=0;i<ar.length;i++){
+      if(ar[i]!=='-'){
+        s=s+parseInt(ar[i]);
+      }
+    }
+    console.log(s,state);
+    
+    if(s%parseInt(state)===0){
+      set3(" Congratulations! You're Lucky Ducky. You're In!")
+
+    }
+    else{
+      set3("Sorry You can't join. You're not Lucky.")
+    }
+
+
+  }
+  
   return (
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="line-1">Are You Lucky Ducky as Donald Duck</h1>
+      <h2 id="line-2">Come join ! May the force be with You</h2>
+    <div>
+      <img src="https://www.fantasiescometrue.com/images/medium/fct_ff3d924055361c6.jpg" alt="Donald Duck Wants You" />
     </div>
+    <h3>Enter Your Birthdate</h3>
+    <input onChange={datepicker} id="box-1" type="date" />
+    <div id="box2-wrapper">   
+       <input onChange={luckypicker} id="box-2" placeholder="Enter your lucky no here" type="text" />
+</div>
+<div id="btn-wrapper">
+  <button onClick={()=>clickhandler()} id="btn-1">Click Here Now !</button>
+</div>
+
+    <h1 id="line-3">{state3}</h1>
+    </div>
+    
   );
 }
 
